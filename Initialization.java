c9fee7feed01f1fe12decabd7bfacd127f8ca2d6
santoshr1982 <santoshr1982@yahoo.com>
2016-03-20 12:45:18 +0530

    declare three variables and initialize one of them.
    This code is written to declare three variables and initialize one in
    single line.

---
+++ Initialization.java
@@ -0,0 +1,14 @@
+package Initialization;
+//This program has been written to declare three variables a,b,c and initialize b to 4 in single line.
+
+public class Initialization {  // Class declaration.
+	
+	public static void main(String[] args) {  //Main method.
+		
+		int iA ,iB = 4 , C ;     //declaring three variables a,b,c and initializing b to 4 in single line.
+		
+		System.out.println(iB); //Printing the value of variable iB.
+	
+	}
+
+}
